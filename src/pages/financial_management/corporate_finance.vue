9bc606c4d63a4e5eacfb7ed0af9e57edbbb225c5
[cdl_chongqing@sina.com] <[cdl_chongqing@sina.com]>
2018-06-09 00:00:09 +0800

    财务

--- src/pages/financial_management/corporate_finance.vue
+++ src/pages/financial_management/corporate_finance.vue
@@ -9,6 +9,58 @@
 <div class="content">
   <h1>财务审批</h1>
   <div class="line"></div>
+  <el-form ref="form" :model="form" label-width="90px" size="mini">
+    <el-col :span="8">
+    <el-form-item label="状态">
+      <el-select v-model="form.status" placeholder="选择状态">
+        <el-option label="已处理" value="shanghai"></el-option>
+        <el-option label="未处理" value="beijing"></el-option>
+      </el-select>
+    </el-form-item>
+    </el-col>
+    <el-col :span="8">
+    <el-form-item label="选择公司">
+      <el-select v-model="form.company" placeholder="选择状态">
+        <el-option label="公司A" value="shanghai"></el-option>
+        <el-option label="公司B" value="beijing"></el-option>
+      </el-select>
+    </el-form-item>
+    </el-col>
+    <el-col :span="8">
+    <el-form-item label="手机或单据" placeholder="选择手机或单据编号">
+      <el-input v-model="form.active"></el-input>
+    </el-form-item>
+    </el-col>
+    <!--<el-form-item class="inline">-->
+      <!--<el-button type="primary" @click="onSubmit">搜索</el-button>-->
+    <!--</el-form-item>-->
+    <el-col :span="8">
+    <el-form-item label="开始时间">
+      <el-date-picker
+        v-model="form.starTime"
+        type="datetime"
+        placeholder="选择开始日期时间"
+        align="right"
+        :picker-options="pickerOptions">
+      </el-date-picker>
+    </el-form-item>
+    </el-col>
+    <el-col :span="8">
+    <el-form-item label="结束时间">
+      <el-date-picker
+        v-model="form.endTime"
+        type="datetime"
+        placeholder="选择结束日期时间"
+        align="right"
+        :picker-options="pickerOptions">
+      </el-date-picker>
+    </el-form-item>
+    </el-col>
+    <el-form-item class="inline">
+      <el-button type="primary" @click="onSubmit">搜索</el-button>
+      <el-button type="primary" @click="onSubmit">重置</el-button>
+    </el-form-item>
+  </el-form>
   <el-table
     :data="tableData"
     border
@@ -16,45 +68,67 @@
     :default-sort = "{prop: 'right', order: 'descending'}"
     >
     <el-table-column
-      prop="name"
-      label="姓名"
+      prop="id"
+      label="公司ID"
       sortable
       >
     </el-table-column>
     <el-table-column
       prop="admin"
-      label="账号"
+      label="单据编号"
       sortable
       >
     </el-table-column>
     <el-table-column
       prop="right"
-      label="权限"
+      label="电话"
       sortable
       >
     </el-table-column>
-    
     <el-table-column
       prop="money"
-      label="余额/¥"
+      label="充值金额"
       sortable
       >
     </el-table-column>
     <el-table-column
       prop="rcmoney"
-      label="充值金额/¥"
+      label="凭证图片"
       sortable
       >
     </el-table-column>
     <el-table-column
-      fixed="right"
+      prop="rcmoney"
+      label="备注"
+      sortable
+      >
+    </el-table-column>
+    <el-table-column
+      prop="rcmoney"
+      label="状态"
+      sortable
+      >
+    </el-table-column>
+    <el-table-column
       label="操作"
       width="90"
       >
       <template slot-scope="scope">
-        <el-button type="primary" @click="showToggle(scope.row)" v-if="scope.row.flag" size="small">确认</el-button>
-        <el-button type="info" @click="showToggle(scope.row)" v-else size="small">完成</el-button>
+        <el-button type="primary" @click="showToggle(scope.row)" v-if="scope.row.flag" size="small">审核通过</el-button>
+        <el-button type="info" @click="showToggle(scope.row)" v-else size="small">不予通过</el-button>
       </template>
+    </el-table-column>
+    <el-table-column
+      prop="rcmoney"
+      label="操作员"
+      sortable
+    >
+    </el-table-column>
+    <el-table-column
+      prop="rcmoney"
+      label="日期"
+      sortable
+    >
     </el-table-column>
   </el-table>
 </div>
@@ -93,17 +167,46 @@
               rcmoney: '4000.00',
               money: '4000.00',
               flag:false
-            }]
+            }],
+            form:{
+              status:'',
+              company:'',
+              active:'',
+              starTime:'',
+              endTime:''
+            },
+            pickerOptions: {
+              shortcuts: [{
+                text: '今天',
+                onClick(picker) {
+                  picker.$emit('pick', new Date());
+                }
+              }, {
+                text: '昨天',
+                onClick(picker) {
+                  const date = new Date();
+                  date.setTime(date.getTime() - 3600 * 1000 * 24);
+                  picker.$emit('pick', date);
+                }
+              }, {
+                text: '一周前',
+                onClick(picker) {
+                  const date = new Date();
+                  date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
+                  picker.$emit('pick', date);
+                }
+              }]
+            },
           }
         },
         methods: {
           confirm() {
             console.log("充值确认");
           },
-          showToggle:function(data){  
+          showToggle:function(data){
             data.flag = !data.flag;
             // console.log(data.flag)
-          }  
+          }
         }
       }
 </script>
@@ -112,4 +215,7 @@
       margin: 20px 0;
       border-top: 1px solid #dcdfe6;
     }
+  .inline{
+    display: inline-block;
+  }
 </style>
