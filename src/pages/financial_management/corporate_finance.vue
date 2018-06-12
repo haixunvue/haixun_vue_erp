ef1c9a78d864d430b961663e1403d09b96b24204
hongjinfeng <hongjinfeng@foisonagel.com>
2018-06-12 15:28:35 +0800

    样式

--- src/pages/financial_management/corporate_finance.vue
+++ src/pages/financial_management/corporate_finance.vue
@@ -112,7 +112,7 @@
     </el-table-column>
     <el-table-column
       label="操作"
-      width="90"
+      width="200"
       >
       <template slot-scope="scope">
         <span v-if="scope.row.process_status=='down'">
