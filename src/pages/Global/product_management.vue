8032c9bb1a738cc7da55d6943789961648996ec5
jq <jq@jqdeMacBook-Pro.local>
2018-06-13 10:12:37 +0800

    8

--- src/pages/Global/product_management.vue
+++ src/pages/Global/product_management.vue
@@ -425,27 +425,33 @@
             pickerOptions: {
                 shortcuts: [{
                   text: '最近一周',
-                  onClick(picker) {
+                  onClick:(picker) =>{
                     const end = new Date();
                     const start = new Date();
                     start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
                     picker.$emit('pick', [start, end]);
+                    this.datetime_end= moment(end).format('YYYY-MM-DD HH:mm:ss') 
+                    this.datetime_start= moment(start).format('YYYY-MM-DD HH:mm:ss') 
                   }
                 }, {
                   text: '最近一个月',
-                  onClick(picker) {
+                  onClick:(picker)=>{
                     const end = new Date();
                     const start = new Date();
                     start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
                     picker.$emit('pick', [start, end]);
+                    this.datetime_end= moment(end).format('YYYY-MM-DD HH:mm:ss') 
+                    this.datetime_start= moment(start).format('YYYY-MM-DD HH:mm:ss') 
                   }
                 }, {
                   text: '最近三个月',
-                  onClick(picker) {
+                  onClick:(picker)=> {
                     const end = new Date();
                     const start = new Date();
                     start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
                     picker.$emit('pick', [start, end]);
+                    this.datetime_end= moment(end).format('YYYY-MM-DD HH:mm:ss') 
+                    this.datetime_start= moment(start).format('YYYY-MM-DD HH:mm:ss') 
                   }
                 }]
               },
